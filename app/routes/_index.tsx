8bdd49cc00dce37d82fa85a0486fd67924f7e0a0
coji <coji@techtalk.jp>
2024-07-14 21:47:19 +0900

    chore: Update form layout and styles in _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,19 +9,16 @@ export default function Post() {
   const actionData = useActionData<typeof action>();
 
   return (
-    <div className="font-sans px-4 py-2 bg-slate-100 min-h-dvh">
-      <h1 className="text-2xl font-bold">Vercel Remix Single Fetch Test</h1>
+    <div className="px-4 py-2">
+      <h1>Vercel Remix Single Fetch Test</h1>
 
-      <Form method="POST" className="mt-4 rounded-lg bg-white shadow px-4 py-5">
-        <button className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400">
+      <Form method="POST">
+        <button className="mt-2 rounded-md bg-indigo-600 px-2.5 py-1.5 font-semibold text-white shadow-sm hover:bg-indigo-500">
           Submit
         </button>
 
         {actionData && (
-          <div>
-            <div className="text-slate-500">action data</div>
-            <div className="border rounded-md p-4">{actionData.message}</div>
-          </div>
+          <div className="mt-2 border rounded p-2">{actionData.message}</div>
         )}
       </Form>
     </div>
