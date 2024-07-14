7701ff83908d9ed89cd665439f91360d51c65afa
coji <coji@techtalk.jp>
2024-07-14 21:44:07 +0900

    chore: Refactor code to use single quotes for imports in root.tsx and _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,76 +1,29 @@
-import { type MetaFunction } from '@vercel/remix';
-import { experimental_useObject as useObject } from '@ai-sdk/react';
-import { schema } from './api';
+import { Form, useActionData } from '@remix-run/react';
+import { unstable_defineAction as defineAction } from '@vercel/remix';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'Recipi Generator' },
-    { name: 'description', content: 'Welcome to Remix!' },
-  ];
-};
+export const action = defineAction(async () => {
+  return { message: 'Hello, world!' };
+});
 
-export default function Index() {
-  const { submit, object, isLoading, error } = useObject({
-    schema,
-    api: '/api',
-  });
+export default function Post() {
+  const actionData = useActionData<typeof action>();
+
   return (
-    <div className="font-sans p-4 bg-slate-100 min-h-dvh">
-      <h1 className="text-3xl">Recipi Generator</h1>
+    <div className="font-sans px-4 py-2 bg-slate-100 min-h-dvh">
+      <h1 className="text-2xl font-bold">Vercel Remix Single Fetch Test</h1>
 
-      <form
-        className="overflow-hidden rounded-lg bg-white shadow px-4 py-5"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.currentTarget);
-          const name = formData.get('name');
-          submit({ name: name ? name.toString() : 'Lasagna' });
-        }}
-      >
-        <div className="flex flex-col gap-2">
-          <label htmlFor="name">Name of dish.</label>
-          <input
-            name="name"
-            placeholder="e.g. Lasagna"
-            className="w-full p-2 border rounded-md"
-          />
-          <button
-            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400"
-            disabled={isLoading}
-          >
-            Generate Recipe
-          </button>
-        </div>
+      <Form method="POST" className="mt-4 rounded-lg bg-white shadow px-4 py-5">
+        <button className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400">
+          Submit
+        </button>
 
-        {!!error && <div className="text-red-500">{String(error)}</div>}
-
-        {object?.recipe && (
-          <div className="flex flex-col gap-2">
-            <div className="mt-4 font-bold">{object.recipe.name}</div>
-            {object.recipe.ingredients && (
-              <div>
-                <div className="text-slate-500">Ingredients</div>
-                <ul className="list-disc list-inside">
-                  {object.recipe.ingredients.map((ingredient, index) => (
-                    <li key={`${index}_${ingredient}`}>{ingredient}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-
-            {object.recipe.steps && (
-              <div>
-                <div className="text-slate-500">Steps</div>
-                <ul className="list-disc list-inside">
-                  {object.recipe.steps.map((step, index) => (
-                    <li key={`${index}_${step}`}>{step}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
+        {actionData && (
+          <div>
+            <div className="text-slate-500">action data</div>
+            <div className="border rounded-md p-4">{actionData.message}</div>
           </div>
         )}
-      </form>
+      </Form>
     </div>
   );
 }
